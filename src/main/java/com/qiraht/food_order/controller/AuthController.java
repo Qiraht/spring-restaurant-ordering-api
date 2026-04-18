7e21d9c530ee5e7f70d9eab309b1e5ef30cad703
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-04-19 01:19:57 +0700

    feat: create AuthController and implement POST /api/auth/login for user login

---
+++ src/main/java/com/qiraht/food_order/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.qiraht.food_order.controller;
+
+import com.qiraht.food_order.dto.ApiResponse;
+import com.qiraht.food_order.dto.request.AuthRequest;
+import com.qiraht.food_order.dto.response.AuthResponse;
+import com.qiraht.food_order.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse<AuthResponse>> postAuthLogin(@Valid @RequestBody AuthRequest request) {
+        AuthResponse data = authService.loginUser(request);
+
+        return ResponseEntity.ok(
+                ApiResponse.success("User login successfully", )
+        )
+    }
+}
