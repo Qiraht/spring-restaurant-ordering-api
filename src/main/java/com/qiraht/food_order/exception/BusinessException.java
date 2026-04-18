1f27bcfcee7c4f851ad9b4140359aab880ca329c
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-04-18 19:02:52 +0700

    feat: create custom exception and implement NotFoundException

---
+++ src/main/java/com/qiraht/food_order/exception/BusinessException.java
@@ -0,0 +1,5 @@
+package com.qiraht.food_order.exception;
+
+public class BusinessException extends RuntimeException{
+    public BusinessException(String message) {super(message);}
+}
---
+++ src/main/java/com/qiraht/food_order/exception/NotFoundException.java
@@ -0,0 +1,5 @@
+package com.qiraht.food_order.exception;
+
+public class NotFoundException extends BusinessException{
+    public NotFoundException(String message) {super(message);}
+}
