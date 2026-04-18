7879c24881c0e5cc632cbf58390f7d5da05385d4
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-04-18 21:29:57 +0700

    feat: implement editMenuById and deleteMenuById on MenuService

--- src/main/java/com/qiraht/food_order/service/MenuService.java
+++ src/main/java/com/qiraht/food_order/service/MenuService.java
@@ -8,6 +8,8 @@ import com.qiraht.food_order.repository.MenuRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -60,5 +62,26 @@ public class MenuService {
                         menu.getPrice(),
                         menu.getStock(),
                         menu.getIsAvailable());
+    }
+
+    public void editMenuById(String id, MenuRequest request) {
+        UUID menuId = UUID.fromString(id);
+
+        Menu menu = menuRepository.findById(menuId).orElseThrow(() -> new NotFoundException("menu with id " + id +" not found"));
+
+        menu.setName(request.name());
+        menu.setDescription(request.description());
+        menu.setPrice(request.price());
+        menu.setStock(request.stock());
+
+        menuRepository.save(menu);
+    }
+
+    public void deleteMenuById(String id) {
+        UUID menuId = UUID.fromString(id);
+
+        Menu menu = menuRepository.findById(menuId).orElseThrow(() -> new NotFoundException("menu with id " + id +" not found"));
+
+        menu.setDeletedAt(LocalDateTime.now());
     }
 }
