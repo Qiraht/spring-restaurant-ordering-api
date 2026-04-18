dd30afdc4742f77517e4c620d3bf7ae063df1c1d
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-04-18 19:02:12 +0700

    feat: add validation for MenuRequest DTO

--- src/main/java/com/qiraht/food_order/dto/request/MenuRequest.java
+++ src/main/java/com/qiraht/food_order/dto/request/MenuRequest.java
@@ -1,11 +1,21 @@
 package com.qiraht.food_order.dto.request;
 
+import jakarta.validation.constraints.*;
+
 import java.math.BigDecimal;
 
 public record MenuRequest(
+        @NotBlank
         String name,
+
         String description,
+
+        @NotNull
+        @DecimalMin(value = "0.01")
         BigDecimal price,
+
+        @NotNull
+        @Min(1)
         Integer stock
 ) {
 }
