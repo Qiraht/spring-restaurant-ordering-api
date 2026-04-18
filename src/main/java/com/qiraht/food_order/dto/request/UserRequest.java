ec93da743a3903d12d58d6a969a8f74c18ab892c
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-04-19 01:05:36 +0700

    feat: create UserRequest dto

---
+++ src/main/java/com/qiraht/food_order/dto/request/UserRequest.java
@@ -0,0 +1,25 @@
+package com.qiraht.food_order.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserRequest(
+        @NotBlank
+        @Size(min = 2, max = 16)
+        String firstName,
+
+        @NotBlank
+        @Size(min = 2, max = 16)
+        String lastName,
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Min(6)
+        String password
+) {
+}
