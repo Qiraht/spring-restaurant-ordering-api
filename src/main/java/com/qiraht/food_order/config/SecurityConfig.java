2a793852ed1c3f9bb98290be3cd236bd10872ba0
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-04-19 01:48:25 +0700

    feat: create SecurityFilterChain on SecurityConfig
    - register public api
    - implements CustomAccessDeniedHandler and UnAuthenticationHandler for SecurityFilterChain exception handling

--- src/main/java/com/qiraht/food_order/config/SecurityConfig.java
+++ src/main/java/com/qiraht/food_order/config/SecurityConfig.java
@@ -1,18 +1,44 @@
 package com.qiraht.food_order.config;
 
+import com.qiraht.food_order.handler.CustomAccessDeniedHandler;
+import com.qiraht.food_order.handler.UnAuthenticationHandler;
+import com.qiraht.food_order.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
+    private final CustomUserDetailsService userDetailsService;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+    private final UnAuthenticationHandler unAuthenticationHandler;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter,
+                          CustomUserDetailsService userDetailsService,
+                          CustomAccessDeniedHandler customAccessDeniedHandler,
+                          UnAuthenticationHandler unAuthenticationHandler
+    ) {
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.userDetailsService = userDetailsService;
+        this.customAccessDeniedHandler= customAccessDeniedHandler;
+        this.unAuthenticationHandler = unAuthenticationHandler;
+    }
+
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
@@ -21,5 +47,28 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .userDetailsService(userDetailsService).authorizeHttpRequests(auth ->
+                        auth.requestMatchers(
+                                        "/api/auth/login",
+                                        "/api/user/register",
+                                        "/v3/api-docs/**",
+                                        "/swagger-ui.html",
+                                        "/swagger-ui/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/menu", "/api/menu/**").permitAll()
+                                .anyRequest().authenticated())
+                .exceptionHandling(ex ->
+                        ex.accessDeniedHandler(customAccessDeniedHandler)
+                                .authenticationEntryPoint(unAuthenticationHandler))
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
