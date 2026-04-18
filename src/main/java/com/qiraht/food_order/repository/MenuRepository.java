3fd581e0e5584d2696b088fc101b9c6d2c10815e
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-04-18 16:23:27 +0700

    feat: create MenuRepository

---
+++ src/main/java/com/qiraht/food_order/repository/MenuRepository.java
@@ -0,0 +1,11 @@
+package com.qiraht.food_order.repository;
+
+import com.qiraht.food_order.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface MenuRepository extends JpaRepository<Menu, UUID> {
+}
