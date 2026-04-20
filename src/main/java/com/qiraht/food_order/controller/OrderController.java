f8b39d7e12101f2f7861386a082e4b768caec2df
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-04-20 18:45:25 +0700

    feat: create OrderController

---
+++ src/main/java/com/qiraht/food_order/controller/OrderController.java
@@ -0,0 +1,18 @@
+package com.qiraht.food_order.controller;
+
+import com.qiraht.food_order.service.OrderService;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+@Validated
+public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+}
