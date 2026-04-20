803d0bb3c997b4f26437edf305b0909d8280e5c0
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-04-20 11:09:41 +0700

    feat: add getAuthenticatedUserByEmail on UserService
    getAuthenticatedUserByEmail is used to get authenticated user entity

--- src/main/java/com/qiraht/food_order/service/UserService.java
+++ src/main/java/com/qiraht/food_order/service/UserService.java
@@ -1,11 +1,14 @@
 package com.qiraht.food_order.service;
 
+import com.qiraht.food_order.config.CustomUserDetails;
 import com.qiraht.food_order.constant.UserRole;
 import com.qiraht.food_order.dto.request.UserRequest;
 import com.qiraht.food_order.entity.User;
 import com.qiraht.food_order.exception.NotFoundException;
 import com.qiraht.food_order.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -42,5 +45,25 @@ public class UserService {
         userRepository.save(user);
 
         return user.getId().toString();
+    }
+
+    public User getAuthenticatedUserByEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new IllegalStateException("No authenticated user found");
+        }
+
+        Object principal = authentication.getPrincipal();
+
+        if (principal instanceof CustomUserDetails userDetails) {
+            return getUserByEmail(userDetails.getUsername());
+        }
+
+        throw new IllegalStateException("Invalid authenticated principal");
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new NotFoundException("User not found"));
     }
 }
