ed02baea635213f4bf4fe1a05fdbf05c217c56d7
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-04-20 11:03:03 +0700

    feat: add Menu Response Mapper

---
+++ src/main/java/com/qiraht/food_order/helper/MenuMapper.java
@@ -0,0 +1,17 @@
+package com.qiraht.food_order.helper;
+
+import com.qiraht.food_order.dto.response.MenuResponse;
+import com.qiraht.food_order.entity.Menu;
+
+public class MenuMapper {
+    public static MenuResponse toResponse(Menu menu) {
+        return new MenuResponse(
+                menu.getId().toString(),
+                menu.getName(),
+                menu.getDescription(),
+                menu.getPrice(),
+                menu.getStock(),
+                menu.getIsAvailable()
+        );
+    }
+}
