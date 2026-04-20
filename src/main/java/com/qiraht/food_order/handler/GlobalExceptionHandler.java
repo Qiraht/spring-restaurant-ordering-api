a61cb778921f009efd6fba29a95ffdad5434a3be
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-04-20 11:08:08 +0700

    feat: implements Manual Validation and BadCredentials Exception on GlobalExceptionHandler and refactor response

--- src/main/java/com/qiraht/food_order/handler/GlobalExceptionHandler.java
+++ src/main/java/com/qiraht/food_order/handler/GlobalExceptionHandler.java
@@ -2,11 +2,13 @@ package com.qiraht.food_order.handler;
 
 import com.qiraht.food_order.dto.ApiResponse;
 import com.qiraht.food_order.exception.NotFoundException;
+import com.qiraht.food_order.exception.ValidationException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -34,15 +36,43 @@ public class GlobalExceptionHandler {
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .orElse("Validation Failed");
 
-        return ResponseEntity.badRequest().body(ApiResponse.failed(message));
+        ApiResponse<Void> body = ApiResponse.failed(message);
+
+        return ResponseEntity.badRequest().body(body);
     }
 
+
+    // Bad Credentials error
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ApiResponse<?>> handleBusinessException(AuthenticationException ex, HttpServletRequest request) {
+        log.error("error while processing request", ex);
+
+        ApiResponse<Void> body = ApiResponse.failed(
+                "Authentication failed/Invalid credentials"
+        );
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(body);
+    }
+
+    // Manual Validation Error
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ApiResponse<?>> handleBusinessException(ValidationException ex, HttpServletRequest request) {
+        log.error("error while processing request", ex);
+
+        ApiResponse<Void> body = ApiResponse.failed(
+                ex.getMessage()
+        );
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(body);
+    }
 
     // Server Error
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Void>> handleBusinessException(Exception ex, HttpServletRequest request) {
         log.error("error while processing request", ex);
 
-        return ResponseEntity.internalServerError().body(ApiResponse.failed(ex.getMessage()));
+        ApiResponse<Void> body = ApiResponse.failed(ex.getMessage());
+
+        return ResponseEntity.internalServerError().body(body);
     }
 }
