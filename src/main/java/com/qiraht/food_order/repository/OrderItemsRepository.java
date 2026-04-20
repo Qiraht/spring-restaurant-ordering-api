4cef3d270ff3ed93260a6c52724f4bcb5f5b7df4
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-04-20 18:44:56 +0700

    feat: create repository for Order and Order Items entity

---
+++ src/main/java/com/qiraht/food_order/repository/OrderItemsRepository.java
@@ -0,0 +1,9 @@
+package com.qiraht.food_order.repository;
+
+import com.qiraht.food_order.entity.OrderItems;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface OrderItemsRepository extends JpaRepository<OrderItems, UUID> {
+}
