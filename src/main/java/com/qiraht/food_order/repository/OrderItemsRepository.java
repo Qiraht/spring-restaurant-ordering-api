13e72db49467f579a5c11166ce29f1f1608d6274
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-04-21 15:40:35 +0700

    feat: add findbyOrder for OrderItemsRepository

--- src/main/java/com/qiraht/food_order/repository/OrderItemsRepository.java
+++ src/main/java/com/qiraht/food_order/repository/OrderItemsRepository.java
@@ -1,9 +1,12 @@
 package com.qiraht.food_order.repository;
 
+import com.qiraht.food_order.entity.Order;
 import com.qiraht.food_order.entity.OrderItems;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface OrderItemsRepository extends JpaRepository<OrderItems, UUID> {
+    List<OrderItems> findByOrder(Order order);
 }
