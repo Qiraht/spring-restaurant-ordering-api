4877f3f689c5f40a8287e488512699edb29bdc9e
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-04-21 18:18:08 +0700

    fix: add missing annotation
    - CartController deleteItemCart missing PreAuthorize
    - Missing Component annotation on UnAuthenticationHandler

--- src/main/java/com/qiraht/food_order/controller/CartController.java
+++ src/main/java/com/qiraht/food_order/controller/CartController.java
@@ -72,6 +72,7 @@ public class CartController {
     }
 
     @DeleteMapping("/{itemId}")
+    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<Void> deleteItemCart(@Valid @PathVariable("itemId") String id) {
         cartService.deleteItemsFromAuthenticationCartById(id);
 
--- src/main/java/com/qiraht/food_order/handler/UnAuthenticationHandler.java
+++ src/main/java/com/qiraht/food_order/handler/UnAuthenticationHandler.java
@@ -6,9 +6,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class UnAuthenticationHandler implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, org.springframework.security.core.AuthenticationException authException) throws IOException, ServletException {
