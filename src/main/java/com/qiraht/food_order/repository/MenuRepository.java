c23c817e88f671b95386a2ac726aa9f51bf4ec0f
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-04-21 15:39:05 +0700

    feat: add findbyIdforUpdate with pessimistic lock on MenuRepository

--- src/main/java/com/qiraht/food_order/repository/MenuRepository.java
+++ src/main/java/com/qiraht/food_order/repository/MenuRepository.java
@@ -1,11 +1,18 @@
 package com.qiraht.food_order.repository;
 
 import com.qiraht.food_order.entity.Menu;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, UUID> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT m FROM Menu m WHERE m.id = :id")
+    Optional<Menu> findByIdForUpdate(UUID id);
 }
