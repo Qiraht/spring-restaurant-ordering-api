0b6ffd192a44ea56784dbe28c6f5b80d402be144
Thariq Aulia Akbar <thariq2712@gmail.com>
2026-04-21 15:41:29 +0700

    feat: add findByUserOrderByCreatedAtDesc
    used to get list of order by user and order it based on createdAt descending

--- src/main/java/com/qiraht/food_order/repository/OrderRepository.java
+++ src/main/java/com/qiraht/food_order/repository/OrderRepository.java
@@ -1,11 +1,14 @@
 package com.qiraht.food_order.repository;
 
 import com.qiraht.food_order.entity.Order;
+import com.qiraht.food_order.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, UUID> {
+    List<Order> findByUserOrderByCreatedAtDesc(User user);
 }
